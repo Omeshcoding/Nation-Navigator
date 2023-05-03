e9f33fb41b3798b4de816cb60faebe8aea2926de
umesh <umesharma.us11@gmail.com>
2023-05-03 20:59:12 +0530

    Refactoring code in homepage

--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import India from '../India.png';
 import { BsArrowLeft } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
